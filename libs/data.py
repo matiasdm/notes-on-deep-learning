235bdc3be7dab6382e9512135c68095fd4c96898
matias di martino <matiasdm@fing.edu.uy>
2019-06-28 09:46:01 -0400

    sss

--- libs/data.py
+++ libs/data.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 def toy_data1(n0=1000, n1=3000):
     """
     Create toy data 1 which consists of two 1-d Gaussian distributions with mean and std:
-    N0(-.5, .4**2) N1(.5, .3**2). Inputs n0 and n1 set the number of samples from class 0 and 1, respectively. The output contain the samples feature values X.shape = (n0+n1, 1) and the samples labels y (shape (n0+n1, 1))
+    # N0(-.5, .4**2) N1(.5, .3**2). Inputs n0 and n1 set the number of samples from class 0 and 1, respectively. The output contain the samples feature values X.shape = (n0+n1, 1) and the samples labels y (shape (n0+n1, 1))
     """
-    X0 = -.5 + .4*np.random.randn(n0,1)  # Gaussian dist with mean -.5 and std .4**2
+    X0 = -.5 + .4*np.random.randn(n0,1)  # Gaussian dist with mean -.5 and std .4**2 jtj g
     X1 = +.5 + .3*np.random.randn(n1,1)  # Gaussian dist with mean +.5 and std .3**2
     y0 = np.zeros((n0,1))
     y1 = np.ones((n1,1))
@@ -32,6 +32,5 @@ if __name__=='__main__':
     # Debug and test stuffs 
     X,y = toy_data1(n0=10, n1=20)
     print(y)
-
 
     
